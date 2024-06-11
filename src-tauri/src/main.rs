b3aac7be1e5f35143570913177e677082709dca5
CyprienGille <>
2024-06-12 00:53:05 +0200

    v0.1.0 COMPLETE

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,24 +2,38 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
+#![allow(clippy::too_many_arguments)]
 
-use serde::Deserialize;
-use serde::Serialize;
-use std::collections::HashSet;
-use std::error::Error;
-use std::fs::File;
-use std::io::BufReader;
-use std::path::Path;
-use std::sync::Mutex;
-use tauri::api::dialog::blocking::FileDialogBuilder;
-use tauri::State;
+use serde::{Deserialize, Serialize};
+use std::{
+    collections::HashSet,
+    error::Error,
+    fs::{write, File},
+    io::BufReader,
+    path::{Path, PathBuf},
+    sync::Mutex,
+};
+use tauri::{api::dialog::blocking::FileDialogBuilder, State};
 
 struct IdState {
     free_id: Mutex<u64>,
 }
 
-struct PersonsState {
+struct UnsavedState {
+    unsaved: Mutex<bool>,
+}
+
+#[derive(Debug)]
+struct NamesState {
     names: Mutex<HashSet<String>>,
+}
+#[derive(Debug)]
+struct GenresState {
+    genres: Mutex<HashSet<String>>,
+}
+#[derive(Debug)]
+struct LocationsState {
+    locations: Mutex<HashSet<String>>,
 }
 
 #[derive(Debug)]
@@ -27,26 +41,37 @@ struct LibraryState {
     medias: Mutex<Vec<Media>>,
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone)]
-struct Person {
-    name: String,
-    role: String,
-}
-
 #[derive(Debug, Default, Clone, Serialize, Deserialize)]
 struct Media {
     id: u64,
     title: String,
-    people: Vec<Person>,
+    author: String,
+    genre: String,
+    copies: u64,
+    year: i64,
+    location: String,
     notes: String,
 }
 
 impl Media {
-    fn new(id: u64, title: String, people: Vec<Person>, notes: String) -> Self {
+    fn new(
+        id: u64,
+        title: String,
+        author: String,
+        genre: String,
+        copies: u64,
+        year: i64,
+        location: String,
+        notes: String,
+    ) -> Self {
         Self {
             id,
             title,
-            people,
+            author,
+            genre,
+            copies,
+            year,
+            location,
             notes,
         }
     }
@@ -55,22 +80,39 @@ impl Media {
 #[tauri::command]
 fn add_media(
     title: String,
-    people: Vec<Person>,
+    author: String,
+    genre: String,
+    copies: u64,
+    year: i64,
+    location: String,
     notes: String,
     library: State<LibraryState>,
     id_state: State<IdState>,
-    all_persons: State<PersonsState>,
+    unsaved_state: State<UnsavedState>,
+    all_names: State<NamesState>,
+    all_genres: State<GenresState>,
+    all_locations: State<LocationsState>,
 ) -> Result<&'static str, &'static str> {
     library.medias.lock().unwrap().push(Media::new(
         *id_state.free_id.lock().unwrap(),
         title,
-        people,
+        author.clone(),
+        genre.clone(),
+        copies,
+        year,
+        location.clone(),
         notes,
     ));
 
     *id_state.free_id.lock().unwrap() += 1;
 
+    all_names.names.lock().unwrap().insert(author);
+    all_genres.genres.lock().unwrap().insert(genre);
+    all_locations.locations.lock().unwrap().insert(location);
+
     println!("{:?}", library);
+
+    *unsaved_state.unsaved.lock().unwrap() = true;
 
     Ok("Addition complete")
 }
@@ -79,9 +121,15 @@ fn add_media(
 fn load_library_file(
     library: State<'_, LibraryState>,
     id_state: State<'_, IdState>,
-    all_persons: State<'_, PersonsState>,
+    unsaved_state: State<'_, UnsavedState>,
+    all_names: State<'_, NamesState>,
+    all_genres: State<'_, GenresState>,
+    all_locations: State<'_, LocationsState>,
 ) {
-    let lib = match FileDialogBuilder::new().pick_file() {
+    let lib = match FileDialogBuilder::new()
+        .add_filter("JSON files", &["json"])
+        .pick_file()
+    {
         Some(fp) => read_lib_from_file(fp).unwrap_or_default(),
         None => Vec::<Media>::default(),
     };
@@ -93,14 +141,33 @@ fn load_library_file(
         .unwrap()
         .iter()
         .max_by_key(|m| m.id)
-        .unwrap_or(&Media::new(0, String::new(), Vec::new(), String::new()))
+        .unwrap_or(&Media::new(
+            0,
+            String::new(),
+            String::new(),
+            String::new(),
+            0,
+            0,
+            String::new(),
+            String::new(),
+        ))
         .id
         + 1;
 
+    *unsaved_state.unsaved.lock().unwrap() = false;
+
     for media in &*library.medias.lock().unwrap() {
-        for ppl in &media.people {
-            all_persons.names.lock().unwrap().insert(ppl.name.clone());
-        }
+        all_names.names.lock().unwrap().insert(media.author.clone());
+        all_genres
+            .genres
+            .lock()
+            .unwrap()
+            .insert(media.genre.clone());
+        all_locations
+            .locations
+            .lock()
+            .unwrap()
+            .insert(media.location.clone());
     }
 }
 
@@ -114,13 +181,105 @@ fn read_lib_from_file<P: AsRef<Path>>(path: P) -> Result<Vec<Media>, Box<dyn Err
 }
 
 #[tauri::command]
+fn save_lib_to_file(library: State<LibraryState>, unsaved_state: State<UnsavedState>) {
+    let path = match FileDialogBuilder::new()
+        .set_file_name("library.json")
+        .save_file()
+    {
+        Some(path) => path,
+        None => PathBuf::from(""), // this ensures failure at the next step if the user closed the file dialog
+    };
+    println!(
+        "Saving status : {:?}",
+        write(
+            path,
+            serde_json::to_string_pretty(&*library.medias.lock().unwrap()).unwrap()
+        )
+    );
+
+    *unsaved_state.unsaved.lock().unwrap() = false;
+}
+
+#[tauri::command]
 fn get_all_medias(library: State<LibraryState>) -> Vec<Media> {
     return (*library.medias.lock().unwrap()).clone();
 }
 
 #[tauri::command]
+fn get_sorted_medias(by: &str, library: State<LibraryState>) -> Vec<Media> {
+    match by {
+        "title" => {
+            (*library.medias.lock().unwrap()).sort_by(|med_1, med_2| med_1.title.cmp(&med_2.title))
+        }
+        "year" => {
+            (*library.medias.lock().unwrap()).sort_by(|med_1, med_2| med_1.year.cmp(&med_2.year))
+        }
+        "author" => (*library.medias.lock().unwrap())
+            .sort_by(|med_1, med_2| med_1.author.cmp(&med_2.author)),
+        &_ => (),
+    }
+    return (*library.medias.lock().unwrap()).clone();
+}
+
+#[tauri::command]
+fn get_unsaved_status(unsaved_state: State<UnsavedState>) -> bool {
+    return *unsaved_state.unsaved.lock().unwrap();
+}
+
+#[tauri::command]
+fn get_filtered_medias(query: String, library: State<LibraryState>) -> Vec<Media> {
+    Vec::from_iter(
+        (*library.medias.lock().unwrap())
+            .clone()
+            .into_iter()
+            .filter(|media| {
+                media.title.contains(&query)
+                    | media.author.contains(&query)
+                    | media.genre.contains(&query)
+                    | media.location.contains(&query)
+                    | media.notes.contains(&query)
+            }),
+    )
+}
+
+#[tauri::command]
+fn get_filtered_names(substring: String, all_names: State<NamesState>) -> Vec<String> {
+    Vec::from_iter(
+        (*all_names.names.lock().unwrap())
+            .clone()
+            .into_iter()
+            .filter(|s| s.contains(&substring)),
+    )
+}
+
+#[tauri::command]
+fn get_filtered_genres(substring: String, all_genres: State<GenresState>) -> Vec<String> {
+    Vec::from_iter(
+        (*all_genres.genres.lock().unwrap())
+            .clone()
+            .into_iter()
+            .filter(|s| s.contains(&substring)),
+    )
+}
+
+#[tauri::command]
+fn get_filtered_locations(substring: String, all_locations: State<LocationsState>) -> Vec<String> {
+    Vec::from_iter(
+        (*all_locations.locations.lock().unwrap())
+            .clone()
+            .into_iter()
+            .filter(|s| s.contains(&substring)),
+    )
+}
+
+#[tauri::command]
 fn get_empty() -> Vec<Media> {
     Vec::new()
+}
+
+#[tauri::command]
+fn get_false() -> bool {
+    false
 }
 
 pub fn main() {
@@ -131,13 +290,31 @@ pub fn main() {
         .manage(IdState {
             free_id: Mutex::new(0),
         })
-        .manage(PersonsState {
+        .manage(UnsavedState {
+            unsaved: Mutex::new(false),
+        })
+        .manage(NamesState {
             names: Mutex::new(HashSet::new()),
+        })
+        .manage(GenresState {
+            genres: Mutex::new(HashSet::new()),
+        })
+        .manage(LocationsState {
+            locations: Mutex::new(HashSet::new()),
         })
         .invoke_handler(tauri::generate_handler![
             load_library_file,
+            save_lib_to_file,
             get_empty,
-            add_media
+            get_false,
+            get_all_medias,
+            get_filtered_medias,
+            get_unsaved_status,
+            get_filtered_names,
+            get_filtered_genres,
+            get_filtered_locations,
+            get_sorted_medias,
+            add_media,
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,58 +1,351 @@
 <script>
-	import Home from '$lib/Home.svelte';
-	import img_library from '$lib/assets/Stray_library.jpeg';
+	import logo from '$lib/assets/obibli.svg';
 	import { invoke } from '@tauri-apps/api/tauri';
 
-	let loaded_data = false;
-
 	let promise = invoke('get_empty');
+	let unsaved_promise = invoke('get_false');
+	let names_promise = invoke('get_empty');
+	let genres_promise = invoke('get_empty');
+	let locations_promise = invoke('get_empty');
 
-	function add_test() {
-		invoke('add_media', {
-			title: 'Les Misérables',
-			people: [{ name: 'Victor Hugo', role: 'Author' }]
-		});
-	}
+	let nav_id = 0; //0 for home, -1 for browse, 1 for add
+	let details_id = -1;
+	let query = '';
+
+	let new_media = {
+		title: '',
+		author: '',
+		genre: '',
+		copies: 0,
+		year: 0,
+		location: '',
+		notes: ''
+	};
 
 	function load_data() {
-		promise = invoke('load_library_file');
-		loaded_data = true;
+		invoke('load_library_file');
+		promise = invoke('get_all_medias');
+		unsaved_promise = invoke('get_unsaved_status');
+	}
+
+	function save_data() {
+		invoke('save_lib_to_file');
+		unsaved_promise = invoke('get_unsaved_status');
+	}
+
+	function go_to_browse() {
+		promise = invoke('get_all_medias');
+		nav_id = -1;
+	}
+	function go_to_add() {
+		nav_id = 1;
+	}
+	function go_home() {
+		nav_id = 0;
+	}
+
+	/**
+	 * @param {number} id
+	 */
+	function display_details(id) {
+		if (details_id == id) {
+			details_id = -1;
+		} else {
+			details_id = id;
+		}
+	}
+
+	/**
+	 * @param {string} by
+	 */
+	function get_sorted(by) {
+		promise = invoke('get_sorted_medias', { by });
+	}
+
+	function add_media() {
+		invoke('add_media', {
+			title: new_media.title,
+			author: new_media.author,
+			genre: new_media.genre,
+			copies: new_media.copies,
+			year: new_media.year,
+			location: new_media.location,
+			notes: new_media.notes
+		});
+		unsaved_promise = invoke('get_unsaved_status');
+		go_home();
+		new_media.title = '';
+		new_media.author = '';
+		new_media.genre = '';
+		new_media.copies = 0;
+		new_media.year = 0;
+		new_media.location = '';
+		new_media.notes = '';
+	}
+
+	/**
+	 * @param {string} query
+	 */
+	function get_search_results(query) {
+		promise = invoke('get_filtered_medias', { query });
+	}
+
+	function reset_search() {
+		query = '';
+		promise = invoke('get_all_medias');
+	}
+
+	function get_filtered_names() {
+		names_promise = invoke('get_filtered_names', { substring: new_media.author });
+	}
+	function get_filtered_genres() {
+		genres_promise = invoke('get_filtered_genres', { substring: new_media.genre });
+	}
+	function get_filtered_locations() {
+		locations_promise = invoke('get_filtered_locations', { substring: new_media.location });
 	}
 </script>
 
-<main class="relative h-screen overflow-hidden bg-orange-800">
-	<img src={img_library} alt="A dusty library" class="absolute z-0 h-full w-full object-cover" />
-	<div class="absolute z-0 h-full w-full bg-black opacity-25" />
-	<div class="absolute z-20 flex w-full justify-evenly">
+<main class="h-full w-full">
+	<div class="mb-2 mt-1 flex w-full justify-evenly">
 		<button
-			class="rounded-md bg-blue-300 p-2 text-lg text-slate-800 hover:bg-blue-500"
+			class="rounded-md bg-blue-300 p-2 text-xl text-slate-800 hover:bg-blue-500"
 			type="button"
 			on:click={load_data}
 		>
 			Charger
 		</button>
-		<button
-			class="rounded-md bg-blue-300 p-2 text-lg text-slate-800 hover:bg-blue-500"
-			type="button"
-		>
-			Sauvegarder
-		</button>
+
+		{#await unsaved_promise then unsaved}
+			{#if unsaved}
+				<button
+					class="rounded-md bg-red-700 p-2 text-xl text-slate-100 hover:bg-red-500"
+					type="button"
+					on:click={save_data}
+				>
+					Sauvegarder
+				</button>
+			{:else}
+				<button
+					class="rounded-md bg-green-400 p-2 text-xl text-slate-900 hover:bg-green-500"
+					type="button"
+					on:click={save_data}
+				>
+					Sauvegarder
+				</button>
+			{/if}
+		{/await}
 	</div>
-	<div class="z-10">
-		{#if loaded_data}
-			{#await promise}
-				<div class="absolute flex h-full w-full items-center justify-center">
-					<div class="rounded bg-slate-900 p-3 text-3xl text-slate-100">Chargement...</div>
-				</div>
-			{:then lib}
-				<Home {lib} />
-			{/await}
-		{:else}
-			<div class="absolute flex h-full w-full items-center justify-center">
-				<div class="rounded bg-slate-900 p-3 text-3xl text-slate-100">
-					⚠️ Chargez une Bibliothèque ⚠️
-				</div>
+	<div class="h-full w-full">
+		{#await promise}
+			<div class="flex h-full w-full items-center justify-center">
+				<div class="rounded bg-slate-900 p-3 text-3xl text-slate-100">Chargement...</div>
 			</div>
-		{/if}
+		{:then lib}
+			<div>
+				{#if nav_id != 0}
+					<div class="justify-normal text-center">
+						<button
+							class="mb-2 mr-4 rounded bg-slate-100 py-2 text-5xl"
+							type="button"
+							on:click={go_home}>🏠</button
+						>
+					</div>
+				{/if}
+
+				{#if nav_id == 0}
+					<div class="flex h-full items-center justify-evenly">
+						<button
+							class="h-full w-full rounded-xl bg-blue-500 px-4 py-4 text-4xl font-bold text-white hover:bg-blue-600"
+							type="button"
+							on:click={go_to_add}
+						>
+							Ajouter
+						</button>
+						<img class="mx-1 h-20 w-auto" src={logo} alt="the obibli logo" />
+						<button
+							class="h-full w-full rounded-xl bg-blue-500 px-4 py-4 text-4xl font-bold text-white hover:bg-blue-600"
+							type="button"
+							on:click={go_to_browse}
+						>
+							Parcourir
+						</button>
+					</div>
+				{:else if nav_id == 1}
+					<!-- Adding UI -->
+					<div class="flex justify-evenly">
+						<div class="">
+							<div class="text-lg font-bold text-slate-900">Titre</div>
+							<input
+								class="rounded-md border-2 border-slate-400 px-3 py-2"
+								type="text"
+								name="title"
+								id="title"
+								bind:value={new_media.title}
+							/>
+						</div>
+						<div>
+							<div class="text-lg font-bold text-slate-900">Auteur</div>
+							<input
+								class="rounded-md border-2 border-slate-400 px-3 py-2"
+								type="text"
+								list="names"
+								id="author"
+								bind:value={new_media.author}
+								on:input={get_filtered_names}
+								on:focus={get_filtered_names}
+							/>
+							<datalist id="names">
+								{#await names_promise}
+									<option value=""></option>
+								{:then names}
+									{#each names as name}
+										<option value={name}>{name}</option>
+									{/each}
+								{/await}
+							</datalist>
+						</div>
+						<div class="">
+							<div class="text-lg font-bold text-slate-900">Année</div>
+							<input
+								class="rounded-md border-2 border-slate-400 px-3 py-2"
+								type="number"
+								id="year"
+								bind:value={new_media.year}
+							/>
+						</div>
+						<div class="">
+							<div class="text-lg font-bold text-slate-900">Copies</div>
+							<input
+								class="rounded-md border-2 border-slate-400 px-3 py-2"
+								type="number"
+								id="copies"
+								bind:value={new_media.copies}
+							/>
+						</div>
+					</div>
+					<div class="flex justify-evenly">
+						<div>
+							<div class="text-lg font-bold text-slate-900">Genre</div>
+							<input
+								class="rounded-md border-2 border-slate-400 px-3 py-2"
+								type="text"
+								list="genres"
+								id="genre"
+								bind:value={new_media.genre}
+								on:input={get_filtered_genres}
+								on:focus={get_filtered_genres}
+							/>
+							<datalist id="genres">
+								{#await genres_promise}
+									<option value=""></option>
+								{:then genres}
+									{#each genres as genre}
+										<option value={genre}>{genre}</option>
+									{/each}
+								{/await}
+							</datalist>
+						</div>
+						<div>
+							<div class="text-lg font-bold text-slate-900">Emplacement</div>
+							<input
+								class="rounded-md border-2 border-slate-400 px-3 py-2"
+								type="text"
+								list="locations"
+								id="location"
+								bind:value={new_media.location}
+								on:input={get_filtered_locations}
+								on:focus={get_filtered_locations}
+							/>
+							<datalist id="locations">
+								{#await locations_promise}
+									<option value=""></option>
+								{:then locations}
+									{#each locations as location}
+										<option value={location}>{location}</option>
+									{/each}
+								{/await}
+							</datalist>
+						</div>
+					</div>
+					<div class="flex justify-evenly">
+						<div>
+							<div class="text-lg font-bold text-slate-900">Notes</div>
+							<textarea
+								class="rounded-md border-2 border-slate-400 p-2"
+								name="notes"
+								id="notes"
+								cols="40"
+								rows="4"
+								bind:value={new_media.notes}
+							></textarea>
+						</div>
+					</div>
+					<div class="flex w-full justify-center">
+						<button
+							class="mt-3 rounded-md bg-green-400 p-3 text-2xl hover:bg-green-500"
+							type="button"
+							on:click={add_media}>Ajouter</button
+						>
+					</div>
+				{:else if nav_id == -1}
+					<!-- Browsing UI -->
+					<div class="mb-2 flex justify-center">
+						<input class="rounded-xl border-2 px-6 py-2" type="text" bind:value={query} />
+						<button class="text-2xl" on:click={() => get_search_results(query)}>🔎</button>
+						<button class="text-2xl" on:click={reset_search}>❌</button>
+					</div>
+					<div class="mx-2 text-slate-800">
+						<div class="flex font-semibold">
+							<div class="w-1/4">
+								<button class="underline" on:click={() => get_sorted('title')}> Titre </button>
+							</div>
+							<div class="w-1/12">
+								<button class="underline" on:click={() => get_sorted('year')}> Année </button>
+							</div>
+							<div class="w-1/4">
+								<button class="underline" on:click={() => get_sorted('author')}> Auteur </button>
+							</div>
+							<div class="w-1/6">Genre</div>
+							<div class="w-1/4">Emplacement</div>
+						</div>
+						{#each lib as media}
+							<div class="rounded border-2">
+								<div class="flex">
+									<div class="w-1/4">
+										<button
+											class="ml-1 text-blue-500 underline"
+											on:click={() => display_details(media.id)}
+										>
+											{media.title}
+										</button>
+									</div>
+									<div class="w-1/12">{media.year}</div>
+									<div class="w-1/4">
+										{media.author}
+									</div>
+									<div class="w-1/6">
+										{media.genre}
+									</div>
+									<div class="w-1/4">
+										{media.location}
+									</div>
+								</div>
+								{#if details_id == media.id}
+									<div class="flex font-semibold">
+										<div class="w-1/4">Copies</div>
+										<div class="w-3/4">Notes</div>
+									</div>
+									<div class="mb-4 flex">
+										<div class="w-1/4">{media.copies}</div>
+										<div class="w-3/4 whitespace-pre-line">{media.notes}</div>
+									</div>
+								{/if}
+							</div>
+						{/each}
+					</div>
+				{/if}
+			</div>
+		{/await}
 	</div>
 </main>
